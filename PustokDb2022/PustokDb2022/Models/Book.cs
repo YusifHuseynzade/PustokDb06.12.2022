dd1cb576c30631ca71f8363b95f574804cb39f10
YusifHuseynzade <110456474+YusifHuseynzade@users.noreply.github.com>
2022-12-06 03:29:55 +0400

    Pustok
    Add

---
+++ PustokDb2022/PustokDb2022/Models/Book.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PustokDb2022.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PustokDb2022.Models
+{
+    public class Book : BaseEntity
+    {
+        public int GenreId { get; set; }
+        public int AuthorId { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        public bool StockStatus { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SalePrice { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DisCountPercent { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal CostPrice { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsSpecial { get; set; }
+        public bool IsNew { get; set; }
+        public Genre Genres { get; set; }
+        public Author Authors { get; set; }
+        public List<BookImage> BookImages { get; set; }
+
+    }
+}
---
+++ PustokDb2022/PustokDb2022/Models/Slider.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using PustokDb2022.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PustokDb2022.Models
+{
+    public class Slider : BaseEntity
+    {
+        [Required]
+        [MaxLength(30)]
+        public string Title1 { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Title2 { get; set; }
+        [Required]
+        [MaxLength(250)]
+        public string Desc { get; set; }
+        [Required]
+        [MaxLength(75)]
+        public string Image { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string BtnText { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string RedirectUrl { get; set; }
+        public int Order { get; set; }
+    }
+
+}
